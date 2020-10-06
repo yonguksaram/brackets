99f347d4f76312ee25bdab8e5c502157e012fcb9
Stanislav Alimpov <stanislav.alimpov@vrpconsulting.com>
2020-10-06 16:40:29 +0300

    feat: implemented sorting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var auxArray = [];
+  for(var i=0;i<str.length;i++){
+    for(var k=0;k<bracketsConfig.length;k++){
+      if(str[i]==bracketsConfig[k][0] && auxArray[auxArray.length-1]!=bracketsConfig[k][1]){
+        auxArray.push(str[i]);
+      } else if(str[i]==bracketsConfig[k][1]){
+        if(auxArray[auxArray.length-1]==bracketsConfig[k][0]){
+          auxArray.pop();
+        } else {
+          return false
+        }
+      }
+    }
+  }
+  return auxArray.length == 0 ? true : false;
 }
